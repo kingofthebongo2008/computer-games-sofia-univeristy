2b0efa859d00793394033f1e97a1c09ce8c5f8a0
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2017-04-07 12:21:06 +0300

    initial import of hello_triangle

---
+++ hello_directx_11/src/hello_triangle/main.cpp
@@ -0,0 +1,176 @@
+#include "pch.h"
+#include <cstdint>
+
+#include <d3d11_3.h>
+#include <dxgi1_5.h>
+#include <wrl/client.h>
+
+#include <winrt/base.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::ApplicationModel::Core;
+using namespace winrt::Windows::ApplicationModel::Activation;
+using namespace Microsoft::WRL;
+
+struct exception : public std::exception
+{
+	exception(HRESULT h) : m_h(h)
+	{
+
+	}
+
+	HRESULT m_h;
+};
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (hr != S_OK)
+	{
+		throw exception(hr);
+	}
+}
+
+static ComPtr<ID3D11Device3> CreateDevice()
+{
+	ComPtr<ID3D11Device> r;
+	D3D_FEATURE_LEVEL levels[]{ D3D_FEATURE_LEVEL_11_0 };
+	ThrowIfFailed(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_DEBUG, &levels[0], 1, D3D11_SDK_VERSION, r.GetAddressOf(), nullptr, nullptr));
+
+	ComPtr<ID3D11Device3> r0;
+	ThrowIfFailed(r.As(&r0));
+	return r0;
+}
+
+static ComPtr<ID3D11DeviceContext> CreateImmediateContext(ID3D11Device* d )
+{
+	ComPtr<ID3D11DeviceContext> r;
+	d->GetImmediateContext(r.GetAddressOf());
+	return r;
+}
+
+static ComPtr<IDXGISwapChain1> CreateSwapChain(const CoreWindow& w, ID3D11Device* d)
+{
+	ComPtr<IDXGIFactory2> f;
+	ComPtr<IDXGISwapChain1> r;
+	
+	ThrowIfFailed(CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG,__uuidof(IDXGIFactory2), reinterpret_cast<void**>(f.GetAddressOf())));
+
+	DXGI_SWAP_CHAIN_DESC1 desc = {};
+
+	desc.BufferCount	= 2;
+	desc.Format			= DXGI_FORMAT_B8G8R8A8_UNORM;
+	desc.Width			= static_cast<UINT>(w.Bounds().Width);
+	desc.Height			= static_cast<UINT>(w.Bounds().Height);
+	desc.SampleDesc.Count = 1;
+	desc.SwapEffect		= DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	desc.BufferUsage	= DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.AlphaMode		= DXGI_ALPHA_MODE_IGNORE;
+	desc.Scaling		= DXGI_SCALING_NONE;
+
+	ThrowIfFailed(f->CreateSwapChainForCoreWindow(d, winrt::get_abi(w), &desc, nullptr, r.GetAddressOf()));
+
+	return r;
+}
+
+static ComPtr<ID3D11RenderTargetView1> CreateSwapChainView(IDXGISwapChain1* swap_chain, ID3D11Device3* device)
+{
+	ComPtr<ID3D11Texture2D> texture;
+
+	ThrowIfFailed(swap_chain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(texture.GetAddressOf())));
+	CD3D11_RENDER_TARGET_VIEW_DESC desc(texture.Get(), D3D11_RTV_DIMENSION_TEXTURE2D);
+	ComPtr<ID3D11RenderTargetView1> r;
+	ThrowIfFailed(device->CreateRenderTargetView1(texture.Get(), nullptr, r.GetAddressOf()));
+	return r;
+}
+
+class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFrameworkViewSource>
+{
+	public:
+
+	IFrameworkView CreateView()
+	{
+			return *this;
+	}
+
+	void Initialize(const CoreApplicationView& v)
+	{
+		m_activated			= v.Activated(winrt::auto_revoke, { this, &ViewProvider::OnActivated });
+		m_device			= CreateDevice();
+		m_device_context	= CreateImmediateContext(m_device.Get());
+	}
+
+	void Uninitialize() 
+	{
+
+	}
+
+	void Run()
+	{
+		while (m_window_running)
+		{
+			CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+			{
+				ComPtr<ID3D11RenderTargetView1> m_swap_chain_view = CreateSwapChainView(m_swap_chain.Get(), m_device.Get());
+
+				//ID3D11RenderTargetView* views[1] = { m_swap_chain_view.Get() };
+				//m_device_context->OMSetRenderTargets(1, views, nullptr);
+
+				float clear_value[4] = { 1.0f, 0.0f, 0.0f, 0.0f };
+				m_device_context->ClearRenderTargetView(m_swap_chain_view.Get(), clear_value);
+			}
+			m_swap_chain->Present(0, 0);
+		}
+	}
+
+	void Load(winrt::hstring_view h)
+	{
+
+	}
+
+	void SetWindow(const CoreWindow& w)
+	{
+		m_closed			= w.Closed(winrt::auto_revoke, { this, &ViewProvider::OnWindowClosed });
+		m_size_changed		= w.SizeChanged(winrt::auto_revoke, { this, &ViewProvider::OnWindowSizeChanged });
+
+		m_swap_chain		= CreateSwapChain(w, m_device.Get());
+
+		//m_swap_chain_view	= CreateSwapChainView(m_swap_chain.Get(), m_device.Get());
+	}
+
+	void OnWindowClosed(const CoreWindow&w, const CoreWindowEventArgs& a)
+	{
+		m_window_running = false;
+	}
+
+	void OnActivated(const CoreApplicationView&, const IActivatedEventArgs&)
+	{
+		CoreWindow::GetForCurrentThread().Activate();
+	}
+
+	void OnWindowSizeChanged(const CoreWindow& w, const WindowSizeChangedEventArgs& a)
+	{
+		ThrowIfFailed(m_swap_chain->ResizeBuffers(3, static_cast<UINT>(a.Size().Width), static_cast<UINT>(a.Size().Height), DXGI_FORMAT_R8G8B8A8_UNORM, 0));
+	}
+
+	bool m_window_running = true;
+	CoreWindow::Closed_revoker					m_closed;
+	CoreWindow::SizeChanged_revoker				m_size_changed;
+	CoreApplicationView::Activated_revoker		m_activated;
+	
+	ComPtr<ID3D11Device3>						m_device;
+	ComPtr<ID3D11DeviceContext>					m_device_context;
+	ComPtr<IDXGISwapChain1>						m_swap_chain;
+};
+
+int32_t __stdcall wWinMain( HINSTANCE, HINSTANCE,PWSTR, int32_t )
+{
+	ThrowIfFailed(CoInitializeEx(nullptr, COINIT_MULTITHREADED));
+	CoreApplication::Run(ViewProvider());
+	CoUninitialize();
+	return 0;
+}
---
+++ hello_directx_11/src/hello_triangle/pch.h
@@ -0,0 +1,10 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <windows.h>
+
+
