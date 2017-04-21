1bcf772d30037275194143da80846f205e8bbc0c
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2017-04-21 13:09:41 +0300

    added hello_triangle_rotate

---
+++ hello_directx_11/src/hello_triangle_rotate/shaders/triangle_vertex.hlsl
@@ -0,0 +1,30 @@
+struct interpolated_value
+{
+	float4 m_position : SV_POSITION;
+};
+
+interpolated_value main(uint v : SV_VERTEXID)
+{
+	interpolated_value r;
+	r.m_position = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	if (v == 0)
+	{
+		r.m_position = float4(0.0f, 0.5f, 0.5f, 1.0f);
+		return r;
+	}
+
+	if (v == 1)
+	{
+		r.m_position = float4(-0.5f, 0.0f, 0.5f, 1.0f);
+		return r;
+	}
+
+	if (v == 2)
+	{
+		r.m_position = float4(0.5f, 0.0f, 0.5f, 1.0f);
+		return r;
+	}
+
+	return r;
+}
