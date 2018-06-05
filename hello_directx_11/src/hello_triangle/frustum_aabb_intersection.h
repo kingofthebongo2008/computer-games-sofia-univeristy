ddd59123bb7add2ef11fe0e81ae5587a7c25cac8
Stefan Dyulgerov <stefan.dyulgerov@ubisoft.com>
2018-06-05 16:20:41 +0300

    split clipper into separate compilation unit

--- hello_directx_11/src/hello_triangle/frustum_aabb_intersection.h
+++ hello_directx_11/src/hello_triangle/frustum_aabb_intersection.h
@@ -3,6 +3,7 @@
 #include <intrin.h>
 #include <cstdint>
 #include <vector>
+#include <array>
 
 namespace computational_geometry
 {
@@ -184,6 +185,99 @@ namespace computational_geometry
         float3 m_max;
     };
 
+    struct plane
+    {
+        float3 m_n;
+        float  m_d = 0.0f;
+    };
+
+    inline plane make_plane(const float3& a, const float3& b, const float3& c)
+    {
+        float3 u = b - a;
+        float3 v = c - a;
+        float3 n = normalize(cross(u, v)); //normal points inside
+        auto     d = -dot(n, a); //note: if the plane equation is ax+by+cz+d, then this is negative, else positive
+        return  { n, d };
+    }
+
+
+    inline std::vector<float3> make_points(const frustum& f)
+    {
+        std::vector<float3> r;
+        r.reserve(8);
+        for (auto i = 0U; i < 8; ++i)
+        {
+            r.push_back(f.m_points[i]);
+        }
+        return r;
+    }
+
+    inline std::array<float3, 8> make_points(const aabb& f)
+    {
+        std::array<float3, 8> r;
+
+        r[0] = { f.m_min.m_x, f.m_min.m_y, f.m_min.m_z };
+        r[1] = { f.m_max.m_x, f.m_min.m_y, f.m_min.m_z };
+        r[2] = { f.m_max.m_x, f.m_max.m_y, f.m_min.m_z };
+        r[3] = { f.m_min.m_x, f.m_max.m_y, f.m_min.m_z };
+
+        r[4] = { f.m_min.m_x, f.m_min.m_y, f.m_max.m_z };
+        r[5] = { f.m_max.m_x, f.m_min.m_y, f.m_max.m_z };
+        r[6] = { f.m_max.m_x, f.m_max.m_y, f.m_max.m_z };
+        r[7] = { f.m_min.m_x, f.m_max.m_y, f.m_max.m_z };
+
+        return r;
+    }
+
+    inline std::array< plane, 6 > make_face_planes(const frustum& f)
+    {
+        std::array< plane, 6 > r;
+
+        //Consistency check, these planes should be like the other ones
+        plane  near0 = make_plane(f.m_points[frustum_points::NearTopRight], f.m_points[frustum_points::NearBottomLeft], f.m_points[frustum_points::NearBottomRight]);
+        plane  far0 = make_plane(f.m_points[frustum_points::FarTopRight], f.m_points[frustum_points::FarBottomRight], f.m_points[frustum_points::FarBottomLeft]);
+        plane  left0 = make_plane(f.m_points[frustum_points::FarTopLeft], f.m_points[frustum_points::NearBottomLeft], f.m_points[frustum_points::NearTopLeft]);
+        plane  right0 = make_plane(f.m_points[frustum_points::FarTopRight], f.m_points[frustum_points::NearBottomRight], f.m_points[frustum_points::FarBottomRight]);
+        plane  top0 = make_plane(f.m_points[frustum_points::FarTopLeft], f.m_points[frustum_points::NearTopRight], f.m_points[frustum_points::FarTopRight]);
+        plane  bottom0 = make_plane(f.m_points[frustum_points::FarBottomLeft], f.m_points[frustum_points::NearBottomRight], f.m_points[frustum_points::NearBottomLeft]);
+
+        r[frustum_planes::Left] = left0;
+        r[frustum_planes::Right] = right0;
+        r[frustum_planes::Top] = top0;
+
+        r[frustum_planes::Bottom] = bottom0;
+        r[frustum_planes::Near] = near0;
+        r[frustum_planes::Far] = far0;
+
+        return r;
+    }
+
+    inline std::array< plane, 6 > make_face_planes(const aabb& f)
+    {
+        std::array< plane, 6 > r;
+        std::array< float3, 8> points = make_points(f);
+
+        //Consistency check, these planes should be like the other ones
+        plane  near0 = make_plane(points[frustum_points::NearTopRight], points[frustum_points::NearBottomLeft], points[frustum_points::NearBottomRight]);
+        plane  far0 = make_plane(points[frustum_points::FarTopRight], points[frustum_points::FarBottomRight], points[frustum_points::FarBottomLeft]);
+        plane  left0 = make_plane(points[frustum_points::FarTopLeft], points[frustum_points::NearBottomLeft], points[frustum_points::NearTopLeft]);
+        plane  right0 = make_plane(points[frustum_points::FarTopRight], points[frustum_points::NearBottomRight], points[frustum_points::FarBottomRight]);
+        plane  top0 = make_plane(points[frustum_points::FarTopLeft], points[frustum_points::NearTopRight], points[frustum_points::FarTopRight]);
+        plane  bottom0 = make_plane(points[frustum_points::FarBottomLeft], points[frustum_points::NearBottomRight], points[frustum_points::NearBottomLeft]);
+
+        r[frustum_planes::Left] = left0;
+        r[frustum_planes::Right] = right0;
+        r[frustum_planes::Top] = top0;
+
+        r[frustum_planes::Bottom] = bottom0;
+        r[frustum_planes::Near] = near0;
+        r[frustum_planes::Far] = far0;
+
+        return r;
+    }
+
     std::vector< float3 > intersection(const frustum& f, const aabb& b);
     std::vector< float3 > clip(const frustum& f, const aabb& b);
+
+
 }
