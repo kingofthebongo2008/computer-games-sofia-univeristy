1c9ef304fe215fc9cb6960aa8f9d1ca33f13afaf
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-03-25 09:00:00 +0200

    moved dx12 skeleton to dx11 skeleton

--- hello_directx_11/src/hello_triangle/pch.h
+++ hello_directx_11/src/hello_triangle/pch.h
@@ -10,8 +10,21 @@
 #include <SDKDDKVer.h>
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
 // Windows Header Files:
+// cppwinrt takes too much time to compile, so it is good to precompile it
 #include <windows.h>
+
+#include <wrl/client.h>
+
+#include <d3d11_3.h>
+#include <dxgi1_5.h>
+
+#include <winrt/base.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.ViewManagement.h>
 
 
 
