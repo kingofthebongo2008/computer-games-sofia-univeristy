7cd141a8b77de0addcf1974eaea250d5e6148ded
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-03-25 08:55:55 +0200

    reworked sample to be simpler

--- hello_directx_12/src/hello_triangle/main.cpp
+++ hello_directx_12/src/hello_triangle/main.cpp
@@ -1,60 +1,13 @@
 #include "pch.h"
 #include <cstdint>
 
-#include <d3d11_3.h>
-#include <dxgi1_5.h>
-#include <wrl/client.h>
-
-#include <winrt/base.h>
-#include <winrt/Windows.UI.Core.h>
-#include <winrt/Windows.ApplicationModel.Core.h>
-#include <winrt/Windows.ApplicationModel.Activation.h>
-#include <winrt/Windows.UI.ViewManagement.h>
-
 #include <triangle_vertex.h>
 #include <triangle_pixel.h>
-
 
 using namespace winrt::Windows::UI::Core;
 using namespace winrt::Windows::ApplicationModel::Core;
 using namespace winrt::Windows::ApplicationModel::Activation;
 using namespace Microsoft::WRL;
-
-struct exception : public std::exception
-{
-	exception(HRESULT h) : m_h(h)
-	{
-
-	}
-
-	HRESULT m_h;
-};
-
-inline void ThrowIfFailed(HRESULT hr)
-{
-	if (hr != S_OK)
-	{
-		throw exception(hr);
-	}
-}
-
-static ComPtr<ID3D11Device3> CreateDevice()
-{
-	ComPtr<ID3D11Device> r;
-	D3D_FEATURE_LEVEL levels[]{ D3D_FEATURE_LEVEL_11_0 };
-	ThrowIfFailed(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_DEBUG, &levels[0], 1, D3D11_SDK_VERSION, r.GetAddressOf(), nullptr, nullptr));
-
-	ComPtr<ID3D11Device3> r0;
-	ThrowIfFailed(r.As(&r0));
-	return r0;
-}
-
-static ComPtr<ID3D11DeviceContext> CreateImmediateContext(ID3D11Device* d )
-{
-	ComPtr<ID3D11DeviceContext> r;
-	d->GetImmediateContext(r.GetAddressOf());
-	return r;
-}
 
 namespace sample
 {
@@ -74,12 +27,46 @@ namespace sample
     }
 }
 
-static ComPtr<IDXGISwapChain1> CreateSwapChain(const CoreWindow& w, ID3D11Device* d)
+struct exception : public std::exception
 {
-	ComPtr<IDXGIFactory2> f;
-	ComPtr<IDXGISwapChain1> r;
+	exception(HRESULT h) : m_h(h)
+	{
+
+	}
+
+	HRESULT m_h;
+};
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (hr != S_OK)
+	{
+		throw exception(hr);
+	}
+}
+
+static winrt::com_ptr<ID3D11Device3> CreateDevice()
+{
+    winrt::com_ptr<ID3D11Device> r;
+	D3D_FEATURE_LEVEL levels[]{ D3D_FEATURE_LEVEL_11_0 };
+	ThrowIfFailed(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_DEBUG, &levels[0], 1, D3D11_SDK_VERSION, r.put(), nullptr, nullptr));
+	return r.as<ID3D11Device3>();
+}
+
+static winrt::com_ptr<ID3D11DeviceContext> CreateImmediateContext(ID3D11Device* d )
+{
+    winrt::com_ptr<ID3D11DeviceContext> r;
+	d->GetImmediateContext(r.put());
+	return r;
+}
+
+
+static winrt::com_ptr<IDXGISwapChain1> CreateSwapChain(const CoreWindow& w, ID3D11Device* d)
+{
+    winrt::com_ptr<IDXGIFactory2> f;
+    winrt::com_ptr<IDXGISwapChain1> r;
 	
-	ThrowIfFailed(CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG,__uuidof(IDXGIFactory2), reinterpret_cast<void**>(f.GetAddressOf())));
+	ThrowIfFailed(CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG,__uuidof(IDXGIFactory2), reinterpret_cast<void**>(f.put())));
 
 	DXGI_SWAP_CHAIN_DESC1 desc = {};
 
@@ -93,39 +80,39 @@ static ComPtr<IDXGISwapChain1> CreateSwapChain(const CoreWindow& w, ID3D11Device
 	desc.AlphaMode		= DXGI_ALPHA_MODE_IGNORE;
 	desc.Scaling		= DXGI_SCALING_NONE;
 
-    ThrowIfFailed(f->CreateSwapChainForCoreWindow(d, sample::copy_to_abi<IUnknown>(w).get(), &desc, nullptr, r.GetAddressOf()));
+    ThrowIfFailed(f->CreateSwapChainForCoreWindow(d, sample::copy_to_abi<IUnknown>(w).get(), &desc, nullptr, r.put()));
 
 	return r;
 }
 
-static ComPtr<ID3D11RenderTargetView1> CreateSwapChainView(IDXGISwapChain1* swap_chain, ID3D11Device3* device)
+static winrt::com_ptr<ID3D11RenderTargetView1> CreateSwapChainView(IDXGISwapChain1* swap_chain, ID3D11Device3* device)
 {
-	ComPtr<ID3D11Texture2D> texture;
+    winrt::com_ptr<ID3D11Texture2D> texture;
 
-	ThrowIfFailed(swap_chain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(texture.GetAddressOf())));
-	CD3D11_RENDER_TARGET_VIEW_DESC desc(texture.Get(), D3D11_RTV_DIMENSION_TEXTURE2D);
-	ComPtr<ID3D11RenderTargetView1> r;
-	ThrowIfFailed(device->CreateRenderTargetView1(texture.Get(), nullptr, r.GetAddressOf()));
+	ThrowIfFailed(swap_chain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(texture.put())));
+	CD3D11_RENDER_TARGET_VIEW_DESC desc(texture.get(), D3D11_RTV_DIMENSION_TEXTURE2D);
+    winrt::com_ptr<ID3D11RenderTargetView1> r;
+	ThrowIfFailed(device->CreateRenderTargetView1(texture.get(), nullptr, r.put()));
 	return r;
 }
 
-static ComPtr<ID3D11VertexShader> CreateTriangleVertexShader(ID3D11Device3* device)
+static winrt::com_ptr <ID3D11VertexShader> CreateTriangleVertexShader(ID3D11Device3* device)
 {
-	ComPtr<ID3D11VertexShader> r;
-	ThrowIfFailed(device->CreateVertexShader(g_triangle_vertex, sizeof(g_triangle_vertex), nullptr, r.GetAddressOf()));
+    winrt::com_ptr<ID3D11VertexShader> r;
+	ThrowIfFailed(device->CreateVertexShader(g_triangle_vertex, sizeof(g_triangle_vertex), nullptr, r.put()));
 	return r;
 }
 
-static ComPtr<ID3D11PixelShader> CreateTrianglePixelShader(ID3D11Device3* device)
+static winrt::com_ptr <ID3D11PixelShader> CreateTrianglePixelShader(ID3D11Device3* device)
 {
-	ComPtr<ID3D11PixelShader> r;
-	ThrowIfFailed(device->CreatePixelShader(g_triangle_pixel, sizeof(g_triangle_pixel), nullptr, r.GetAddressOf()));
+    winrt::com_ptr <ID3D11PixelShader> r;
+	ThrowIfFailed(device->CreatePixelShader(g_triangle_pixel, sizeof(g_triangle_pixel), nullptr, r.put()));
 	return r;
 }
 
-static ComPtr<ID3D11RasterizerState2> CreateRasterizerState(ID3D11Device3* device)
+static winrt::com_ptr<ID3D11RasterizerState2> CreateRasterizerState(ID3D11Device3* device)
 {
-	ComPtr<ID3D11RasterizerState2> r;
+    winrt::com_ptr<ID3D11RasterizerState2> r;
 
 	D3D11_RASTERIZER_DESC2 state = {};
 
@@ -135,28 +122,28 @@ static ComPtr<ID3D11RasterizerState2> CreateRasterizerState(ID3D11Device3* devic
 	state.DepthClipEnable		= TRUE;
 	state.ScissorEnable			= TRUE;
 
-	ThrowIfFailed(device->CreateRasterizerState2(&state, r.GetAddressOf()));
+	ThrowIfFailed(device->CreateRasterizerState2(&state, r.put()));
 	return r;
 }
 
-static ComPtr<ID3D11BlendState1> CreateBlendState(ID3D11Device3* device)
+static winrt::com_ptr<ID3D11BlendState1> CreateBlendState(ID3D11Device3* device)
 {
-	ComPtr<ID3D11BlendState1> r;
+    winrt::com_ptr<ID3D11BlendState1> r;
 
 	D3D11_BLEND_DESC1 state = {};
 	state.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
-	ThrowIfFailed(device->CreateBlendState1(&state, r.GetAddressOf()));
+	ThrowIfFailed(device->CreateBlendState1(&state, r.put()));
 	return r;
 }
 
-static ComPtr<ID3D11DepthStencilState> CreateDepthStencilState(ID3D11Device3* device)
+static winrt::com_ptr<ID3D11DepthStencilState> CreateDepthStencilState(ID3D11Device3* device)
 {
-	ComPtr<ID3D11DepthStencilState> r;
+    winrt::com_ptr <ID3D11DepthStencilState> r;
 
 	D3D11_DEPTH_STENCIL_DESC state = {};
 	state.DepthEnable = FALSE;
 	state.DepthFunc = D3D11_COMPARISON_LESS;
-	ThrowIfFailed(device->CreateDepthStencilState(&state, r.GetAddressOf()));
+	ThrowIfFailed(device->CreateDepthStencilState(&state, r.put()));
 	return r;
 }
 
@@ -173,7 +160,7 @@ class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFra
 	{
 		m_activated			= v.Activated(winrt::auto_revoke, { this, &ViewProvider::OnActivated });
 		m_device			= CreateDevice();
-		m_device_context	= CreateImmediateContext(m_device.Get());
+		m_device_context	= CreateImmediateContext(m_device.get());
 	}
 
 	void Uninitialize() 
@@ -188,27 +175,27 @@ class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFra
 			CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
 
 			{
-				ComPtr<ID3D11RenderTargetView1> m_swap_chain_view = CreateSwapChainView(m_swap_chain.Get(), m_device.Get());
+                winrt::com_ptr<ID3D11RenderTargetView1> m_swap_chain_view = CreateSwapChainView(m_swap_chain.get(), m_device.get());
 
 				m_device_context->ClearState();
 
 				{
-					float clear_value[4] = { 1.0f, 0.0f, 0.0f, 0.0f };
-					m_device_context->ClearRenderTargetView(m_swap_chain_view.Get(), clear_value);
+					float clear_value[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+					m_device_context->ClearRenderTargetView(m_swap_chain_view.get(), clear_value);
 				}
 
 				{
-					ID3D11RenderTargetView* views[1] = { m_swap_chain_view.Get() };
+					ID3D11RenderTargetView* views[1] = { m_swap_chain_view.get() };
 					m_device_context->OMSetRenderTargets(1, views, nullptr);
-					m_device_context->OMSetDepthStencilState(m_depth_stencil_state.Get(), 0);
+					m_device_context->OMSetDepthStencilState(m_depth_stencil_state.get(), 0);
 				}
 
 				{
-					m_device_context->OMSetBlendState(m_blend_state.Get(), nullptr, 0xFFFFFFFF);
+					m_device_context->OMSetBlendState(m_blend_state.get(), nullptr, 0xFFFFFFFF);
 				}
 
 				{
-					m_device_context->RSSetState(m_rasterizer_state.Get());
+					m_device_context->RSSetState(m_rasterizer_state.get());
 
 					D3D11_RECT r = { 0, 0, m_back_buffer_width, m_back_buffer_height };
 					m_device_context->RSSetScissorRects(1, &r);
@@ -226,11 +213,11 @@ class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFra
 				}
 
 				{
-					m_device_context->VSSetShader(m_triangle_vertex.Get(), nullptr, 0);
+					m_device_context->VSSetShader(m_triangle_vertex.get(), nullptr, 0);
 				}
 
 				{
-					m_device_context->PSSetShader(m_triangle_pixel.Get(), nullptr, 0);
+					m_device_context->PSSetShader(m_triangle_pixel.get(), nullptr, 0);
 				}
 				{
 					m_device_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
@@ -247,12 +234,12 @@ class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFra
 
 	void Load(winrt::hstring h)
 	{
-		m_triangle_vertex = CreateTriangleVertexShader(m_device.Get());
-		m_triangle_pixel = CreateTrianglePixelShader(m_device.Get());
+		m_triangle_vertex = CreateTriangleVertexShader(m_device.get());
+		m_triangle_pixel = CreateTrianglePixelShader(m_device.get());
 
-		m_blend_state = CreateBlendState(m_device.Get());
-		m_rasterizer_state = CreateRasterizerState(m_device.Get());
-		m_depth_stencil_state = CreateDepthStencilState(m_device.Get());
+		m_blend_state = CreateBlendState(m_device.get());
+		m_rasterizer_state = CreateRasterizerState(m_device.get());
+		m_depth_stencil_state = CreateDepthStencilState(m_device.get());
 	}
 
 	void SetWindow(const CoreWindow& w)
@@ -260,7 +247,7 @@ class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFra
 		m_closed			= w.Closed(winrt::auto_revoke, { this, &ViewProvider::OnWindowClosed });
 		m_size_changed		= w.SizeChanged(winrt::auto_revoke, { this, &ViewProvider::OnWindowSizeChanged });
 
-		m_swap_chain		= CreateSwapChain(w, m_device.Get());
+		m_swap_chain		= CreateSwapChain(w, m_device.get());
 
 		m_back_buffer_width = static_cast<UINT>(w.Bounds().Width);
 		m_back_buffer_height = static_cast<UINT>(w.Bounds().Height);
@@ -285,25 +272,24 @@ class ViewProvider : public winrt::implements<ViewProvider, IFrameworkView, IFra
 	}
 
 	bool m_window_running = true;
+
 	CoreWindow::Closed_revoker					m_closed;
 	CoreWindow::SizeChanged_revoker				m_size_changed;
 	CoreApplicationView::Activated_revoker		m_activated;
 	
-	ComPtr<ID3D11Device3>						m_device;
-	ComPtr<ID3D11DeviceContext>					m_device_context;
-	ComPtr<IDXGISwapChain1>						m_swap_chain;
+    winrt::com_ptr <ID3D11Device3>				m_device;
+    winrt::com_ptr <ID3D11DeviceContext>		m_device_context;
+    winrt::com_ptr <IDXGISwapChain1>			m_swap_chain;
 
-	ComPtr<ID3D11VertexShader>					m_triangle_vertex;
-	ComPtr<ID3D11PixelShader>					m_triangle_pixel;
-	ComPtr<ID3D11RasterizerState2>				m_rasterizer_state;
-	ComPtr<ID3D11BlendState1>					m_blend_state;
-	ComPtr<ID3D11DepthStencilState>				m_depth_stencil_state;
+    winrt::com_ptr <ID3D11VertexShader>			m_triangle_vertex;
+    winrt::com_ptr <ID3D11PixelShader>			m_triangle_pixel;
+    winrt::com_ptr <ID3D11RasterizerState2>		m_rasterizer_state;
+    winrt::com_ptr <ID3D11BlendState1>			m_blend_state;
+    winrt::com_ptr <ID3D11DepthStencilState>	m_depth_stencil_state;
 
 	uint32_t									m_back_buffer_width = 0;
 	uint32_t									m_back_buffer_height = 0;
 
-	
-	
 };
 
 int32_t __stdcall wWinMain( HINSTANCE, HINSTANCE,PWSTR, int32_t )
