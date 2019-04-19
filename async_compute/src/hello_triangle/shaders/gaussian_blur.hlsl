8a8cb2a13b2eefd795d4fa55ac67be493f8af4a8
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-04-19 17:29:25 +0300

    fixed blur

--- async_compute/src/hello_triangle/shaders/gaussian_blur.hlsl
+++ async_compute/src/hello_triangle/shaders/gaussian_blur.hlsl
@@ -1,11 +1,20 @@
 #include "default_signature.hlsli"
 
-RWTexture2D<float4>   lightingBuffer : register( u0 );
+globallycoherent RWTexture2D<float4>   lightingBuffer : register( u0 );
 
-[numthreads(16, 16, 1)]
+groupshared uint4 pixels[ 18 ];
+
+[numthreads(8, 8, 1)]
 [RootSignature( MyRS2 ) ]
-void main( uint3 DTid    : SV_DispatchThreadID )
+void main( uint3 DTid    : SV_DispatchThreadID, uint3 gtid : SV_GroupThreadID )
 {
+	/*
+	uint2 size;
+	lightingBuffer.GetDimensions(size.x, size.y);
+
+	if ( gtid.xy == uint2( 0, 0 ) )
+	GroupMemoryBarrierWithGroupSync();
+	*/	
 	lightingBuffer[DTid.xy] = float4(0,0.5,0.5,1.0);
 }
 
