c9b57a935177d41376eb5d50167aee58bfc95c17
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2019-06-10 08:00:23 +0300

    sample engine

--- task_system/engine/SampleEngine/Program.cpp
+++ task_system/engine/SampleEngine/Program.cpp
@@ -24,103 +24,197 @@ struct world_transform
 	float m_scale;
 };
 
-namespace renderer
+class renderer_component;
+
+class renderer_component_handle
 {
-	class component;
-
-	class component_handle
-	{
-		component* m_pointer;
-	};
-
-	struct render_object_allocator
-	{
-		void* make_object( type )
-		{
-
-		}
-
-		void free_object(type t)
-		{
-
-		}
-	};
-
-	struct visibility_object_allocator
-	{
-		void* make_object( type )
-		{
-
-		}
-
-		void free_object( type t )
-		{
-
-		}
-	};
-}
-
-namespace game
-{
-	class object
-	{
-		public:
-
-		virtual std::vector< renderer::component_handle > make_components( render_object_allocator* r, visibility_object_allocator* r) = 0;
-	};
-}
-
-namespace visibility
-{
-	class object;
-}
-
-namespace renderer
-{
-	class object;
-
-	class component
-	{
-		object*					m_render_object;
-		visibility::object*		m_visibility_object;
-	};
-
-	class object
-	{
-		component*				m_render_component;
-	};
-
-	class world
-	{
-		std::vector<object*>	m_objects;
-	};
-}
-
-namespace visibility
-{
-	class object
-	{
-		renderer::object*	m_render_object;
-	};
-
-	class world
-	{
-		std::vector< object* > m_objects;
-	};
-}
-
-class mechanic	final : public game::object
-{
-	renderer::component_handle m_render_components[1];
+	renderer_component* m_pointer;
 };
 
-class room final : public game::object
+struct render_object_allocator
 {
-	renderer::component_handle m_render_components[17];
+	enum type
+	{
+		mechanic,
+		room,
+		count
+	};
+
+	template <typename o >
+	o* make_object( type )
+	{
+		return nullptr;
+	}
+
+	template <typename o>
+	void free_object(type t, o* o )
+	{
+
+	}
 };
 
+struct visibility_object_allocator
+{
+	enum type
+	{
+
+	};
+
+	template <typename o >
+	o* make_object(type)
+	{
+		return nullptr;
+	}
+
+	template <typename o>
+	void free_object(type t, o* o)
+	{
+		
+	}
+};
+
+struct scratch_pad_allocator
+{
+	void* allocate(size_t s) { return nullptr; };
+};
+
+void* operator new (std::size_t size, scratch_pad_allocator* ptr) throw()
+{
+	return ptr->allocate(size);
+}
+
+void* f()
+{
+	scratch_pad_allocator* p;
+	return new (p) float;
+}
+
+struct scratch_pad_deleter
+{
+	void operator()(void*) const
+	{
+
+	}
+};
+
+template <typename t> std::unique_ptr<t, scratch_pad_deleter > make_unique( scratch_pad_allocator* p )
+{
+	return unique_ptr<t, scratch_pad_deleter >(new (p) t());
+}
+
+struct gpu_resources_allocator
+{
+	void* create_texture_2d() { return nullptr; };
+	void* create_buffer() { return nullptr; };
+	void* create_geometry() { return nullptr; };
+};
+
+struct gpu_command_list
+{
+
+};
+
+struct make_components_allocators
+{
+	render_object_allocator* m_roa;
+	visibility_object_allocator* m_voa;
+	scratch_pad_allocator* m_soa;
+	gpu_resources_allocator* m_ga;
+	gpu_command_list* m_cmd_list;
+};
+
+class game_object
+{
+	public:
+
+	std::vector< renderer_component_handle >		 make_components(make_components_allocators* allocators) { return on_make_components(allocators); }
+
+	protected:
+
+	virtual ~game_object() {};
+	
+	private:
+
+	virtual std::vector< renderer_component_handle > on_make_components(make_components_allocators* allocators) = 0;
+};
+
+class visibility_object;
+class renderer_object;
+
+class renderer_component
+{
+	renderer_object*				m_render;
+	visibility_object*				m_visibility;
+};
+
+class renderer_object
+{
+	public:
+	game_object*					m_game_object;
+};
+
+class renderer_world
+{
+	public:
+	std::vector<renderer_object*>	m_objects;
+};
+
+class visibility_object
+{
+	renderer_object*	m_render;
+};
+
+class visibility_world
+{
+	public:
+	std::vector< visibility_object* > m_objects;
+};
+
+class mechanic	final : public game_object
+{
+	private:
+
+	class mechanic_render_object final : public renderer_object
+	{
+
+	};
+
+	std::vector< renderer_component_handle > on_make_components(make_components_allocators* allocators) override
+	{
+		std::vector< renderer_component_handle > res;
+		return res;
+	}
+
+	renderer_component_handle m_render_components[1];
+};
+
+class room final : public game_object
+{
+	private:
+
+	std::vector< renderer_component_handle > on_make_components(make_components_allocators* allocators) override
+	{
+		std::vector< renderer_component_handle > res;
+		return res;
+	}
+
+	renderer_component_handle m_render_components[17];
+};
 
 int main()
 {
+	make_components_allocators  alloc;
 
+	renderer_world				rw;
+	visibility_world			vw;
+
+	room						room;
+	mechanic					mechanic;
+	
+	{
+		for (auto&& c : room.make_components(&alloc))
+		{
+			
+		}
+	}
 }
