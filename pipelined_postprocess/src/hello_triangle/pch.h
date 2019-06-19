9f55e04ccd23d1e604d18d4040ac4896dd515300
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-06-19 18:18:04 +0300

    fix pipelined postprocess

--- pipelined_postprocess/src/hello_triangle/pch.h
+++ pipelined_postprocess/src/hello_triangle/pch.h
@@ -23,6 +23,7 @@
 
 #include <winrt/base.h>
 #include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Windows.UI.ViewManagement.h>
