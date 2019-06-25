b8ffc58a53ebf66bd1e4309c19945093a6c6bca7
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2019-06-25 08:15:54 +0300

    tiled resources mars

---
+++ tiled-resources-mars/push.bat
@@ -0,0 +1,2 @@
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tiled-resources-mars-normal.1.0.0.nupkg --timeout 10000
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tiled-resources-mars-diffuse.1.0.0.nupkg --timeout 10000
