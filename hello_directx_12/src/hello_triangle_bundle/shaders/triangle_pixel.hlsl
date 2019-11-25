0bcad2031f74c8ba39ff22fdca1c9ae226d861bf
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-11-25 16:54:10 +0200

    added constants
    added constants

--- hello_directx_12/src/hello_triangle_bundle/shaders/triangle_pixel.hlsl
+++ hello_directx_12/src/hello_triangle_bundle/shaders/triangle_pixel.hlsl
@@ -6,6 +6,14 @@ struct interpolated_value
     float3 m_color    : TEXCOORD0;
 };
 
+//36 DWORDS, must match the root signature
+cbuffer Constants : register(b9)
+{
+	float m_w;
+	float m_h;
+	float m_pad[2];
+};
+
 [RootSignature( MyRS3 ) ]
 float4 main(interpolated_value v) : SV_TARGET0
 {
