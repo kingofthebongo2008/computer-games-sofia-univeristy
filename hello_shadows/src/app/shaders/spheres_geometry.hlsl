c77ca2c91638b6d0ac2fe00e72bd8fb64a721615
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2020-03-11 14:50:50 +0200

    fixes

--- hello_shadows/src/app/shaders/spheres_geometry.hlsl
+++ hello_shadows/src/app/shaders/spheres_geometry.hlsl
@@ -14,25 +14,39 @@ struct vertex_input
 	float4 m_position_os  : POSITION;
 };
 
-[maxvertexcount(6)]
+[maxvertexcount(4)]
 void    main(point vertex_input input[1], inout TriangleStream<interpolated_value> outputStream )
 {
 	float3	camera_position = m_camera_position.xyz;// -float3 (m_view[0].w, m_view[1].w, m_view[2].w);
 	float halfWidth			= 0.5;
 
+	/*
+
 	float3 forward			= normalize(input[0].m_position_os.xyz - camera_position);
 	float3 up				= float3(0.0f, 1.0f, 0.0f);
-	float3 right			= normalize(cross(forward, up));
+	float3 temp				= normalize(cross(up, forward));
+	float3 right			= normalize(cross(up, temp));
 
 	right					= right * halfWidth;
 	up						= normalize( cross(right, forward) ) * halfWidth;
+	*/
+
+	float3 up				= float3(0.0f, 1.0f, 0.0f);
+	float3 eye_direction	= normalize(input[0].m_position_os.xyz - camera_position);
+	float3 r2				= normalize(eye_direction);
+	float3 r0				= normalize(cross(up, eye_direction));
+	float3 r1				= cross(r0, r2);
+	float3 negate			= -camera_position;
+	
+	float3 right2			= r0	* halfWidth;
+	float3 up2				= r1	* halfWidth;
 
 	float3 vert[4];
 
-	vert[0]					= input[0].m_position_os.xyz - right - up; // Get bottom left vertex
-	vert[1]					= input[0].m_position_os.xyz + right - up; // Get bottom right vertex
-	vert[2]					= input[0].m_position_os.xyz - right + up; // Get top left vertex
-	vert[3]					= input[0].m_position_os.xyz + right + up; // Get top right vertex
+	vert[0]					= input[0].m_position_os.xyz - right2 - up2; // Get bottom left vertex
+	vert[1]					= input[0].m_position_os.xyz + right2 - up2; // Get bottom right vertex
+	vert[2]					= input[0].m_position_os.xyz - right2 + up2; // Get top left vertex
+	vert[3]					= input[0].m_position_os.xyz + right2 + up2; // Get top right vertex
 
 	float2 texCoord[4];
 	texCoord[0] = float2(-1, -1);
@@ -58,10 +72,11 @@ void    main(point vertex_input input[1], inout TriangleStream<interpolated_valu
 	outputStream.Append(v1);
 	outputStream.Append(v0);
 	outputStream.Append(v3);
-	outputStream.RestartStrip();
-	outputStream.Append(v3);
-	outputStream.Append(v0);
 	outputStream.Append(v2);
+	//outputStream.RestartStrip();
+	//outputStream.Append(v3);
+	//outputStream.Append(v0);
+	//outputStream.Append(v2);
 
 	/*
 	vertex_attributes o0;
