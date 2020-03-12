f2a7d722c0001619216a205279a52d07409ffbba
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2020-03-12 14:28:57 +0200

    remove redundant stuff

--- hello_shadows/src/app/shaders/spheres_pixel.hlsl
+++ hello_shadows/src/app/shaders/spheres_pixel.hlsl
@@ -12,12 +12,5 @@ float4 main(interpolated_value v) : SV_TARGET0
     float   alpha = 1;
     float3  color = float3(0, 1, 0) * alpha;
 
-    float3 n      = float3(v.m_clip_space.x, v.m_clip_space.y, 0);
-    float r2      = dot(n.xy, n.xy);
-
-    // if the texel is not inside the sphere
-    //if (r2 > 1.0f)
-      //  discard;
-
     return float4(color, alpha);
 }
